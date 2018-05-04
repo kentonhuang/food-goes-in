87b62de227ea9d74d082fc7e7483f1e7b680c40e
Kenton Huang <huangkenton@gmail.com>
2018-05-03 21:05:36 -0700

    key

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ const config = {
 		YELP_KEY: process.env.YELP_KEY
 	},
 	development: {
-		YELP_KEY: 'kv2SXo2kqTdnhXCwQyrkmPUOdnYTvUTFjvpFoot22iy-XBVIvJ-oovs-YdY4FMZp3_UMAyY6bLT7CznFp6XB74VWGJ7-edkN1v-f3pDiMm3FuhogxaTvDOdKQdS2WnYx'
+		YELP_KEY: 'hihi'
 	}
 }
 
